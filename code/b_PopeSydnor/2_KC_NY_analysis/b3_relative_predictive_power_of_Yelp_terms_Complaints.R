997234d08bf3b6396e4b07ae7b242723af9002cf
Kristen M. Altenburger <kristen@DN525eou.SUNet>
2018-06-27 16:23:15 -0700

    update

--- code/b_PopeSydnor/2_KC_NY_analysis/b3_relative_predictive_power_of_Yelp_terms_Complaints.R
+++ code/b_PopeSydnor/2_KC_NY_analysis/b3_relative_predictive_power_of_Yelp_terms_Complaints.R
@@ -10,13 +10,11 @@ mean(( kang_df_rmse$Full_RF_RMSE_train_without_CP - kang_df_rmse$Full_RF_RMSE_tr
 #[1] -0.0003659654
 
 
-### still re-running 6/24 
-
 ## compute relative % decrease in RMSE when adding CP to model for New York
 ny_df_rmse <- read.csv('./output/NYC_output_with_without_complaints.csv')
 
 # for RF
 mean(( ny_df_rmse$Full_RF_RMSE_test_without_CP - ny_df_rmse$Full_RF_RMSE_test )/ ny_df_rmse$Full_RF_RMSE_test)
-#[1] 0.0001899566
+#[1] 0.0001822071
 mean(( ny_df_rmse$Full_RF_RMSE_train_without_CP - ny_df_rmse$Full_RF_RMSE_train )/ ny_df_rmse$Full_RF_RMSE_train)
-#[1] 0.001642343
+#[1] 0.002227397
